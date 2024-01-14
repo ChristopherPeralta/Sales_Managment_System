70d8338decc6431de432f27893dc9fcb2ef749ef
ChristopherPeralta <cperaltapenaloza@gmail.com>
2024-01-14 09:36:17 -0500

    CustomerService_ServiceImplement
    List of customer,
    Search customer by ID,
    Save customer,
    Delete customer

---
+++ src/main/java/com/sales/services/CustomerService.java
@@ -0,0 +1,20 @@
+package com.sales.services;
+
+import com.sales.dto.Request.CustomerRequestDTO;
+import com.sales.dto.Response.CustomerResponseDTO;
+import com.sales.entities.Customer;
+
+import java.util.List;
+
+public interface CustomerService {
+    public List<CustomerResponseDTO> getAllCustomers();
+
+    public CustomerResponseDTO getCustomerById(Long id);
+
+    public CustomerResponseDTO saveCustomer(CustomerRequestDTO customerRequestDTO);
+
+    public CustomerResponseDTO updateCustomer(CustomerRequestDTO customerRequestDTO, Long id);
+
+    public void deleteCustomer(Long id);
+
+}
---
+++ src/main/java/com/sales/services/impl/CustomerImpl.java
@@ -0,0 +1,69 @@
+package com.sales.services.impl;
+
+import com.sales.dto.Request.CustomerRequestDTO;
+import com.sales.dto.Response.CustomerResponseDTO;
+import com.sales.dto.mapper.CustomerMapper;
+import com.sales.entities.Customer;
+import com.sales.repositories.CustomerRepository;
+import com.sales.services.CustomerService;
+import lombok.AllArgsConstructor;
+import lombok.NoArgsConstructor;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
+
+@Transactional
+@AllArgsConstructor
+@Service
+public class CustomerImpl implements CustomerService {
+
+    private final CustomerRepository customerRepository;
+    private final CustomerMapper customerMapper;
+
+    //Listar
+    @Transactional(readOnly = true)
+    @Override
+    public List<CustomerResponseDTO> getAllCustomers() {
+        List<Customer> customersList = customerRepository.findAll();
+        return customerMapper.customerResponseDTOListDTOList(customersList);
+    }
+
+    @Override
+    public CustomerResponseDTO getCustomerById(Long id) {
+        Customer customer = customerRepository.findById(id)
+                .orElseThrow(() -> new IllegalArgumentException("El cliente no existe." + id));
+        return customerMapper.toDTO(customer);
+    }
+
+    @Override
+    public CustomerResponseDTO saveCustomer(CustomerRequestDTO customerRequestDTO) {
+        Customer customer = customerMapper.customerRequestDTOtoCustomer(customerRequestDTO);
+        customer.setActive(true);
+        Customer customerSaved = customerRepository.save(customer);
+        return customerMapper.toDTO(customerSaved);
+    }
+
+    @Override
+    public CustomerResponseDTO updateCustomer(CustomerRequestDTO customerRequestDTO, Long id) {
+        Customer customer = customerRepository.findById(id)
+                .orElseThrow(() -> new IllegalArgumentException("El cliente no existe." + id));
+
+        customer.setName(customerRequestDTO.getName());
+        customer.setAddress(customerRequestDTO.getAddress());
+        customer.setEmail(customerRequestDTO.getEmail());
+        customer.setPhone(customerRequestDTO.getPhone());
+        customer.setDocumentNumber(customerRequestDTO.getDocumentTypeName());
+
+        Customer customerSaved = customerRepository.save(customer);
+        return customerMapper.toDTO(customerSaved);
+    }
+
+    @Override
+    public void deleteCustomer(Long id) {
+        Customer customer = customerRepository.findById(id)
+                .orElseThrow(() -> new IllegalArgumentException("El cliente no existe." + id));
+        customer.setActive(false);
+        customerRepository.save(customer);
+    }
+}
