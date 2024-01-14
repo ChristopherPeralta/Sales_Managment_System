fcc1b03d3ac0367d5feb7be235e6781661d705d2
ChristopherPeralta <cperaltapenaloza@gmail.com>
2024-01-14 09:38:20 -0500

    CustomerResponse
    id, documentNumber, name, phone, address, email, isActive, documentTypeName

--- src/main/java/com/sales/dto/Response/CustomerResponseDTO.java
+++ src/main/java/com/sales/dto/Response/CustomerResponseDTO.java
@@ -1,4 +1,27 @@
 package com.sales.dto.Response;
 
+import lombok.*;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
 public class CustomerResponseDTO {
+
+    private Long id;
+
+    private String documentNumber;
+
+    private String name;
+
+    private String phone;
+
+    private String address;
+
+    private String email;
+
+    private boolean isActive;
+
+    private String documentTypeName;
 }
