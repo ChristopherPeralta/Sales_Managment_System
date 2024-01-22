339f501a01e81b6fcfe7fd28c067f662e82f22df
ChristopherPeralta <cperaltapenaloza@gmail.com>
2024-01-22 08:31:24 -0500

    RepositoryDistrict&Province

---
+++ src/main/java/com/sales/repositories/DistrictRepository.java
@@ -0,0 +1,14 @@
+package com.sales.repositories;
+
+import com.sales.entities.District;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface DistrictRepository extends JpaRepository<District, Long> {
+
+    @Query("SELECT dt FROM district dt WHERE LOWER(dt.name) = LOWER(:name)")
+    District findByNameIgnoreCase(@Param("name") String name);
+}
---
+++ src/main/java/com/sales/repositories/ProvinceRepository.java
@@ -0,0 +1,14 @@
+package com.sales.repositories;
+
+import com.sales.entities.Province;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface ProvinceRepository extends JpaRepository<Province, Long> {
+
+    @Query("SELECT dt FROM province dt WHERE LOWER(dt.name) = LOWER(:name)")
+    Province findByNameIgnoreCase(@Param("name") String name);
+}
