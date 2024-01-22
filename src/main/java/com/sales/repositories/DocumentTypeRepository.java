75d93a829035a6121c9c68937fa6e8bfda42683e
ChristopherPeralta <cperaltapenaloza@gmail.com>
2024-01-22 08:31:42 -0500

    DocumentTypeRepository

--- src/main/java/com/sales/repositories/DocumentTypeRepository.java
+++ src/main/java/com/sales/repositories/DocumentTypeRepository.java
@@ -4,7 +4,9 @@ import com.sales.entities.DocumentType;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface DocumentTypeRepository extends JpaRepository<DocumentType, Long> {
     @Query("SELECT dt FROM DocumentType dt WHERE LOWER(dt.name) = LOWER(:name)")
     DocumentType findByNameIgnoreCase(@Param("name") String name);
